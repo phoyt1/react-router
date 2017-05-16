34b9d3ed6b3b64f7f3e7fb4a3d8338703a760a71
Patrick Hoyt <ptrckhoyt120@gmail.com>
2017-05-16 17:44:16 -0400

    child routes

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
+import Albums from './components/Albums';
+import Album from './components/Album';
 
 ReactDOM.render(
-  <AppContainer />,
+  <Router history={hashHistory}>
+    <Route path='/' component={AppContainer} >
+      <IndexRedirect to= '/albums' />
+      <Route path='/albums' component={Albums} />
+      <Route path='/album' component={Album} />
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
